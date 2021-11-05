21acd0ef096b3d3faa07dc6c9e5af8da37fc77bb
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-05 17:43:31 +0100

    ?

---
+++ src/Ejercicio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    /*public static void main(String[] args) {*/
+      /*  int edad;
+        System.out.println("Introduzca su edad");
+        Scanner escaneado = new Scanner(System.in);
+        edad = escaneado.nextInt();
+        if (edad >=18) {
+            System.out.println("Mayor de edad");}
+        if (edad < 18) {
+            System.out.println("Menor de edad");}*/
+    } public static void escaner(int edad){
+        System.out.println("Introduzca su edad");
+        Scanner escaneado = new Scanner(System.in);
+        edad = escaneado.nextInt();
+    }public static void
+        escaner(if ( edad >=18) {
+            System.out.println("Mayor de edad");}
+        if (edad < 18) {
+            System.out.println("Menor de edad");}} );
+
+    public static void todoJunto(){
+        escaner(operacion
+    }
+}
