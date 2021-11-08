3cf4d4576bb7b0cf3316e421529fd68d296ad903
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-08 19:32:34 +0100

    ?

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
-    /*public static void main(String[] args) {*/
+    public static void main(String[] args) {
+        int edad;
+        System.out.println("Introduzca su edad");
+        Scanner escaneado = new Scanner(System.in);
+        edad = escaneado.nextInt();
       /*  int edad;
         System.out.println("Introduzca su edad");
         Scanner escaneado = new Scanner(System.in);
@@ -10,17 +14,63 @@ public class Ejercicio1 {
             System.out.println("Mayor de edad");}
         if (edad < 18) {
             System.out.println("Menor de edad");}*/
-    } public static void escaner(int edad){
-        System.out.println("Introduzca su edad");
-        Scanner escaneado = new Scanner(System.in);
-        edad = escaneado.nextInt();
-    }public static void
-        escaner(if ( edad >=18) {
-            System.out.println("Mayor de edad");}
-        if (edad < 18) {
-            System.out.println("Menor de edad");}} );
+        public static void calculo{
 
-    public static void todoJunto(){
-        escaner(operacion
+        escaner(if (edad >= 18) {
+            System.out.println("Mayor de edad");
+        }
+        if (edad < 18) {
+            System.out.println("Menor de edad");
+        }
+
+
     }
 }
+
+
+
+
+/* imprime(string cadena, int veces){
+for(int i=0; i<veces;i++);
+sout(cadena);
+
+main
+sout print ("primera linea")
+imprime("hola", 2)
+sout print("segunda linea")
+
+
+public static boolean esPrimo(int numero){
+boolean esPrimo=true;
+for (int i=2; i <= numero/2; i++{
+if (numero%i==0){
+esPrimo = false;
+break;}
+}
+return esPrimo;
+}
+
+
+public static void main{
+for (int i=3; i<100; i++)
+if(esPrimo(i))
+sout(i);
+
+
+public static int elDoble(int numero){
+return numero * 2;
+
+pub sta void repiteNVeces( String cadena, int veces, boolean enMayusculas){
+for (int i=0; i<veces; i++)
+if (enMaysuculas)
+sout(cadena.toUppercase());
+else
+sout (cadena);
+
+pub stat void main
+repiteNVeces(cadena:"hola", veces 2, enMayusculas: true);}
+
+
+pub stat void main
+repiteNVeces("hola".toUppercase, veces 2,);
+ */
