07694726699d6e1074d8043434812735ebc59360
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-10 20:03:11 +0100

    ?

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+
 public class Ejercicio6 {
+    public static boolean esDivisible(double n,double m){
+        boolean si=false;
+        if ((n/m)>0){
+            si = true;
+       }
+        return si;
+    }
     public static void main(String[] args) {
+        Scanner escaneado= new Scanner(System.in);
+        System.out.println("Introduzca el primer numero");
+        double n=escaneado.nextInt();
+        System.out.println("Introduzca el segundo numero");
+        double m=escaneado.nextInt();
+
 
     }
 }
