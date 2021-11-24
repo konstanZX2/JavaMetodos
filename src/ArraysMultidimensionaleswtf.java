3765b7ee72f8fa9543a93164a4f73923e8c2ee83
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-24 19:52:01 +0100

    ?

--- src/ArraysMultidimensionaleswtf.java
+++ src/ArraysMultidimensionaleswtf.java
@@ -2,5 +2,9 @@ public class ArraysMultidimensionaleswtf {
     public static void main(String[] args) {
         filas son []
                 columnas son la segunda[]
+            0 1 2 3 4 5 6
+        2
+        1
+        0
     }
 }
