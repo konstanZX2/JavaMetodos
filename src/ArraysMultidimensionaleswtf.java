4a6fb34a66f53a17ce819b74f1fb96511d1f44f8
konstanZX2 <zxgenerationzx@gmail.com>
2021-11-24 19:53:55 +0100

    ?

--- src/ArraysMultidimensionaleswtf.java
+++ src/ArraysMultidimensionaleswtf.java
@@ -2,8 +2,9 @@ public class ArraysMultidimensionaleswtf {
     public static void main(String[] args) {
         filas son []
                 columnas son la segunda[]
+                por ejemplo= punto (2,3)
             0 1 2 3 4 5 6
-        2
+        2         X
         1
         0
     }
